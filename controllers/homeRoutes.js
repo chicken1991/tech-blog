1594a6d1e8e64058e42e2f188e42ff11f8405c86
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-07-12 21:04:14 -0700

    building routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,30 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', 
-
-
-)
+router.get('/', async (req, res) => {
+    try {
+      // Get all posts and JOIN with user data
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((Post) => Post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        posts, 
+        // logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
