916880d8b5008adb270de9806dd134e60967c3c0
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-07-14 21:05:13 -0700

    signup works

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -39,6 +39,18 @@ router.get('/', async (req, res) => {
     });
   });
 
+  router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('signup',{
+    // logged_in: req.session.logged_in 
+    });
+  });
+
   router.get('/dashboard', async (req, res) => {
     console.log(req.session.user_id);
     try {
